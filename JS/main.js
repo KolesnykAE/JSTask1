6ee3a6163385833a761f32a83f0100c8ac359f64
KolesnykAE <kolesnik.ae@gmail.com>
2021-03-25 19:58:17 +0200

    Initial commit

---
+++ JS/main.js
@@ -0,0 +1,460 @@
+// 1. - присвоить каждому из следующих значений свою переменную:
+//     'hello', 'owu', 'com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false
+// Вывести каждую при помощи console.log , alert, document.write
+//
+//     let Gretting = 'hello';
+//     let abbr = 'owu';
+//     let domain = 'com';
+//     let country = 'ua';
+//     let a = 1;
+//     let b = 10;
+//     let c = -999;
+//     let d = 123;
+//     let fractA = 3.14;
+//     let fractB = 2.7;
+//     let e = 16;
+//     let found = true;
+//     let lost = false;
+//
+//         console.log(Gretting);
+//         console.log(abbr);
+//         console.log(domain);
+//         console.log(country);
+//         console.log(a);
+//         console.log(b);
+//         console.log(c);
+//         console.log(d);
+//         console.log(fractA);
+//         console.log(fractB);
+//         console.log(e);
+//         console.log(found);
+//         console.log(lost);
+
+    // комбинация ctrl + D - в файле JS копирует рядок но не вниз а рядом - она всегда так в JS р
+
+        // alert(Gretting);
+        // alert(abbr);
+        // alert(domain);
+        // alert(country);
+        // alert(a);
+        // alert(b);
+        // alert(c);
+        // alert(d);
+        // alert(fractA);
+        // alert(fractB);
+        // alert(e);
+        // alert(found);
+        // alert(lost);
+
+        // document.write(Gretting + '<br>');
+        // document.write(abbr + '<br>');
+        // document.write(domain + '<br>');
+        // document.write(country + '<br>');
+        // document.write(a + '<br>');
+        // document.write(b + '<br>');
+        // document.write(c + '<br>');
+        // document.write(d + '<br>');
+        // document.write(fractA + '<br>');
+        // document.write(fractB + '<br>');
+        // document.write(e + '<br>');
+        // document.write(found + '<br>');
+        // document.write(lost);
+
+// 2. - переопределить каждую переменную из задания 1 дав им произвольные значения Вывести каждую при помощи console.log , alert, document.write
+
+    // Greeting = "Hi";
+    // abbr = "nau";
+    // domain = "gmail";
+    // country = "it";
+    // a = 5;
+    // b = 9;
+    // c = 583;
+    // d = -18;
+    // fractA = 5.8;
+    // fractB = 4.75;
+    // e = true;
+    // found = "search";
+    // lost = "waste";
+    //
+    //     console.log(Gretting);
+    //     console.log(abbr);
+    //     console.log(domain);
+    //     console.log(country);
+    //     console.log(a);
+    //     console.log(b);
+    //     console.log(c);
+    //     console.log(d);
+    //     console.log(fractA);
+    //     console.log(fractB);
+    //     console.log(e);
+    //     console.log(found);
+    //     console.log(lost);
+
+        // alert(Gretting);
+        // alert(abbr);
+        // alert(domain);
+        // alert(country);
+        // alert(a);
+        // alert(b);
+        // alert(c);
+        // alert(d);
+        // alert(fractA);
+        // alert(fractB);
+        // alert(e);
+        // alert(found);
+        // alert(lost);
+
+        // document.write(Gretting + '<br>');
+        // document.write(abbr + '<br>');
+        // document.write(domain + '<br>');
+        // document.write(country + '<br>');
+        // document.write(a + '<br>');
+        // document.write(b + '<br>');
+        // document.write(c + '<br>');
+        // document.write(d + '<br>');
+        // document.write(fractA + '<br>');
+        // document.write(fractB + '<br>');
+        // document.write(e + '<br>');
+        // document.write(found + '<br>');
+        // document.write(lost);
+
+// 3. - Создать 3 числовых и 3 стринговых константы. Вывести каждую при помощи console.log , alert, document.write
+
+    // const rate = 3;
+    // const number = 5;
+    // const count = 8;
+    // const name = 'Elena';
+    // const hobby = 'skating';
+    // const color = 'green';
+    //
+    //     console.log(rate);
+    //     console.log(number);
+    //     console.log(count);
+    //     console.log(name);
+    //     console.log(hobby);
+    //     console.log(color);
+    //
+    //     alert(rate);
+    //     alert(number);
+    //     alert(count);
+    //     alert(name);
+    //     alert(hobby);
+    //     alert(color);
+    //
+    //     document.write(rate + '<br>');
+    //     document.write(number + '<br>');
+    //     document.write(count + '<br>');
+    //     document.write(name + '<br>');
+    //     document.write(hobby + '<br>');
+    //     document.write(color + '<br>');
+
+// 4. - при помощи 3х разных prompt() получить 3 слова которые являются вашими ФИО.
+//     Для фамилии имени и отчества создать разные переменные. Вывести каждую при помощи console.log , alert, document.write
+
+    // let surname = prompt('enter your surname');
+    // let name = prompt('enter your name');
+    // let secondName = prompt('enter your second name');
+    // // //
+    //     console.log(surname);
+    //     console.log(name);
+    //     console.log(secondName);
+    //
+    //     alert(surname);
+    //     alert(name);
+    //     alert(secondName);
+    //
+    //     document.write(surname + '<br>');
+    //     document.write(name + '<br>');
+    //     document.write(secondName + '<br>');
+
+// 5. - Взять переменные из задания 4 и сконкатенировать их в одной переменной person.
+
+    // let person1 = surname + ' ' + name + ' ' + secondName;
+    //
+    //     console.log(person1);
+
+// 6. - Взять задние 4 и 5 и применить его к ФИО всех членов своей семьи.
+//
+//     let MotherSurname = prompt("enter Mother's surname");
+//     let MotherName = prompt("enter Mother's name");
+//     let MotherSecondName = prompt("enter Mother's second name");
+//
+//         console.log(MotherSurname);
+//         console.log(MotherName);
+//         console.log(MotherSecondName);
+//
+//         alert(MotherSurname);
+//         alert(MotherName);
+//         alert(MotherSecondName);
+//
+//         document.write(MotherSurname);
+//         document.write(MotherName);
+//         document.write(MotherSecondName);
+//
+//     let FatherSurname = prompt("enter Father's surname");
+//     let FatherName = prompt("enter Father's name");
+//     let FatherSecondName = prompt("enter Father's second name");
+//
+//         console.log(FatherSurname);
+//         console.log(FatherName);
+//         console.log(FatherSecondName);
+//
+//         alert(FatherSurname);
+//         alert(FatherName);
+//         alert(FatherSecondName);
+//
+//         document.write(FatherSurname);
+//         document.write(FatherName);
+//         document.write(FatherSecondName);
+//
+//     let BrotherSurname = prompt("enter Brother's surname");
+//     let BrotherName = prompt("enter Brother's name");
+//     let BrotherSecondName = prompt("enter Brother's second name");
+//
+//         console.log(BrotherSurname);
+//         console.log(BrotherName);
+//         console.log(BrotherSecondName);
+//
+//         alert(BrotherSurname);
+//         alert(BrotherName);
+//         alert(BrotherSecondName);
+//
+//         document.write(BrotherSurname);
+//         document.write(BrotherName);
+//         document.write(BrotherSecondName);
+
+// 7. - при помощи prompt() получить 3 числа. Привести их к числовому типу при помощи +. вывести их в консоль.
+
+    // let num1 = +prompt('enter number1');
+    // let num2 = +prompt('enter number2');
+    // let num3 = +prompt('enter number2');
+    //
+    //     console.log(num1);
+    //     console.log(num2);
+    //     console.log(num3);
+
+// 8. - при помощи prompt() получить 4 числа. Привести их к числовому типу при помощи parseInt.
+//     Сложить их между собой записав результат в переменную result и вывести в консоль браузера
+
+    // let num133 = prompt('enter number 133');
+    // let num65 = prompt('enter number 65');
+    // let num9 = prompt('enter number 9');
+    // let num188 = prompt('enter number 188');
+    //
+    // let myNum = parseInt(num133);
+    // let myNum1 = parseInt(num65);
+    // let myNum2 = parseInt(num9);
+    // let myNum3 = parseInt(num188);
+    //
+    // let result = myNum + myNum1 + myNum2 + myNum3;
+    //
+    // console.log(result);
+
+// 9. - при помощи prompt()  получить 3 числа с плавающей точекой. при помощи parseFloat привести их к соответсвующему типу.
+//     Сложить их между собой записав результат в переменную result и вывести в консоль браузера
+
+    // let numA = prompt(3.14);
+    // let numB = prompt(9.3);
+    // let numC = prompt(55.788);
+    //
+    // let myNumA = parseFloat(numA);
+    // let myNumB = parseFloat(numB);
+    // let myNumC = parseFloat(numC);
+    //
+    // let result = myNumA + myNumB + myNumC;
+    //
+    // console.log(result);
+
+// 10. - при помощи prompt()  получить 3 числа с плавающей точекой. Округлить их при помощи Math.round
+// Сложить их между собой записав результат в переменную  и вывести в консоль браузера
+
+    // let numX = prompt('enter number 5.3');
+    // let numK = prompt('enter number 33.3');
+    // let numL = prompt('enter number 9.81');
+    //
+    // let M1 = Math.round(numX);
+    // let M2 = Math.round(numK);
+    // let M3 = Math.round(numL);
+    //
+    // let result = M1 + M2 + M3;
+    //
+    // console.log(result);
+
+// 11. - при помощи prompt()  получить 2 целых числа. Привести их к целочисленному типу. Первое число - это число которое будут возводить в степень.
+//     Второе число - это число которое является степенью. При помощи Math.pow возвести первое число в степень второго числа.
+
+    // let numY = parseInt(prompt('enter number 3'));
+    // let numZ = parseInt(prompt('enter number 8'));
+    //
+    // let result = Math.pow(numY, numZ);
+    //
+    // console.log(result);
+
+// 12. - При помощи оператора опредеоения типа typeof определить типы следующих переменных
+// и вывести их в консоль let a = 100; let b = '100'; let c = true; let d = undefined;
+
+    // let a = 100;
+    // console.log(typeof a);
+    //
+    // let b = '100';
+    // console.log(typeof b);
+    //
+    // let c = true;
+    // console.log(typeof c);
+    //
+    // let d = undefined;
+    // console.log(typeof undefined);
+
+// 13. - Поставьте соответствующий оператор в выражениях что бы получился соответсвующий результат.
+//     В одинаковых выражениях в задании не использовать два одинаковых оператора.
+//     Каждое выражение вложить в свою переменную
+
+    // 5 < 6 -> true
+    // 5 > 6 -> false
+    // 5 = 6 -> false
+    // 5 >= 6 -> false
+    // 10 = 10 -> true
+    // 10 === 10 -> true
+    // 10 > 10 -> false
+    // 10 < 10 -> false
+    // 10 != 10 -> false
+    // 123 === '123' -> false
+    // 123 == '123' -> true
+
+// Дополнительно:
+//     - Посмотрев на следюующие выражения, скажите, каков будет вывод в консоль
+
+// Логический &&: expr1 && expr2 - Если expr1 будет true, тов возвращает expr2; если, expr1 false то возвращает expr1
+
+    // console.log(132 > 100 && 45 < 12 );  -> false
+    // console.log(34 > 33 && 23 < 90 );  -> true
+    // console.log(99 > 100 && 45 > 12 );  -> false
+
+// Логический ||: expr1 || expr2 - Если expr1 будет true, тов возвращает expr1; если, expr1 - false то возвращает expr2
+
+    // console.log(132 > 100 || 45 < 12 );  -> true
+    // console.log(111 > 11 || 45 < 111 );  -> true
+
+// по приоритету && исполняеться перед оператором ||
+
+    // console.log((111 > 11 || 45 < 111) && (132 > 100 || 45 < 12) );  -> true
+    // console.log((111 > 11 || 45 < 56) || (132 > 100 || 45 < 12) );  -> true
+    // console.log((132 > 100 && 45 < 12 ) && (132 > 100 || 45 < 12 ) );  -> false
+    // console.log((111 > 11 || 45 < 111) || (99 > 100 && 45 > 12 ));  -> true
+
+// truthy - все, кроме false, 0, -0, 0n, "", null, undefined, NaN
+
+    // console.log(!!'-1'); ->true
+    // console.log(!!-1); ->true
+    // console.log(!!'0'); ->true
+    // console.log(!!'null'); ->true
+    // console.log(!!'undefined'); ->true
+    // console.log(!!(3/'owu')); -> false
+    // console.log((111 > 11 || 45 < 111) ||  !!'0'); ->true
+    // console.log((!!111 == !!11 || 45 < 111) && (99 > 100 && 45 > 12 )); -> false
+
+
+// CLASS
+
+// 1.Определите переменные str, num, flag и txt со значениями «Привет», 123, true, «true».
+// При помощи оператора typeof убедитесь, что значения переменных принадлежат типам: string, number и  boolean.
+
+    // let str = 'Привет'
+    // let num = 123;
+    // let flag = true;
+    // let txt = 'true';
+    //
+    // console.log(typeof str);
+    // console.log(typeof num);
+    // console.log(typeof flag);
+    // console.log(typeof txt);
+
+// 2. Создайте переменные a1, a2, a3, a4, a5. При помощи математических операторов (сложение, вычитание и т.д.) найдите значения выражений:
+//     5 + 3,
+//     5 - 3,
+//     5 * 3,
+//     5 / 3,
+//     5 % 3
+// поместив результат каждого выражения в соответствующую переменную. Например, let a1 = 5 + 3.
+
+    // let a1 = 5+3;
+    // console.log(a1);
+    //
+    // let a2 = 5-3;
+    // console.log(a2);
+    //
+    // let a3 = 5*3;
+    // console.log(a3);
+    //
+    // let a4 = 5/3;
+    // console.log(a4);
+    //
+    // let a5 = 5%3;
+    // console.log(a5);
+
+// 3.  Создайте переменные a6, a7, a8, a9, a10. Поместите в них результат выражений:
+//     5 % 3,
+//     3 % 5,
+//     5 + '3',
+//     '5' - 3,
+//     75 + 'кг'
+
+    // let a6 = 5 % 3;
+    // console.log(a6);
+    //
+    // let a7 = 3 % 5;
+    // console.log(a7);
+    //
+    // let a8 = 5 + '3';
+    // console.log(a8);
+    //
+    // let a9 = '5' - 3;    (почему если от стринги отнять число то выводит число?)
+    // console.log(a9);
+    //
+    // let a10 = 75 + 'кг'
+    // console.log(a10);
+
+// 4. Напишите код, который находит площадь прямоугольника высота 23см. ( переменная height),
+// шириной 10см (переменная width), значение площади должно хранится в числовой переменной s.
+
+    // let height = 23;
+    // let width = 10;
+    // let s = height * width;
+    //
+    // console.log(s);
+
+// 5.  Напиши код, который находит объем цилиндра высотой 10м (переменная heightC) и диаметром основания 4м (dC),
+//     результат поместите в переменную v.
+
+    // let heightC = 10;
+    // let dC = 4;
+    // let r = dC / 2;
+    // let pi = 3.14;
+    // let v = pi*r**2*heightC;
+    //
+    // console.log(v);
+
+// 7. Напишите скрипт, который выводит "Hello world", создавши переменную str и выводить спомощью document.write, alert и console.log
+
+    // let str = "Hello world"
+    //
+    // document.write(str);
+    //
+    // alert(str);
+    //
+    // console.log(str);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
